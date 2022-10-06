eb5119db2c83b97ead9460dce4cb9cd80adcfc3b
JKTAkwada <67474166+JKTAkwada@users.noreply.github.com>
2022-10-06 21:35:56 +0000

    Add files via upload

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+st.set_page_config(page_tittle="Retail Area Analysis", page_icon=":tada:")
+
+# ----- Header Section ------
+st.subheader("Hi, I am Josh :wave:")
+st.title("A Data Engineer from Gahna")
+st.write("Learn more")
+
+
